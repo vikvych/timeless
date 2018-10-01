82514d1be7dc6968758b038695b1bcf3940213b9
Ivan Tkachenko <ivan.tknko@gmail.com>
2018-10-01 12:16:57 +0300

    Initial Commit

---
+++ Timeless/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Timeless
+//
+//  Created by Ivan Tkachenko on 10/1/18.
+//  Copyright © 2018 Ivan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
